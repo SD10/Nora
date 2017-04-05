3bbf5cb7101ade375ddb53f0f7c0c469bb3593a2
Steven Deutsch <stevensdeutsch@yahoo.com>
2017-04-04 19:15:23 -0500

    Add StorageResponse

---
+++ Nora/StorageResponse.swift
@@ -0,0 +1,42 @@
+//
+//  StorageResponse.swift
+//  Nora
+//
+//  Created by Steven on 4/4/17.
+//  Copyright © 2017 NoraFirebase. All rights reserved.
+//
+
+import Foundation
+import FirebaseStorage
+
+// MARK: - StorageResponse
+
+public struct StorageResponse {
+    
+    let data: Data?
+    let metaData: FIRStorageMetadata?
+    let url: URL?
+}
+
+
+extension StorageResponse {
+    
+    init(data: Data) {
+        self.data = data
+        self.metaData = nil
+        self.url = nil
+    }
+    
+    init(url: URL) {
+        self.url = url
+        self.metaData = nil
+        self.data = nil
+    }
+    
+    init(metaData: FIRStorageMetadata) {
+        self.metaData = metaData
+        self.url = nil
+        self.data = nil
+    }
+    
+}
