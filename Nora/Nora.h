a71f40710b55d377c149e61dadb7d081f5f16aa1
Steven Deutsch <stevensdeutsch@yahoo.com>
2017-04-04 19:01:31 -0500

    Initial Commit

---
+++ Nora/Nora.h
@@ -0,0 +1,19 @@
+//
+//  Nora.h
+//  Nora
+//
+//  Created by Steven on 4/4/17.
+//  Copyright © 2017 NoraFirebase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nora.
+FOUNDATION_EXPORT double NoraVersionNumber;
+
+//! Project version string for Nora.
+FOUNDATION_EXPORT const unsigned char NoraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nora/PublicHeader.h>
+
+
