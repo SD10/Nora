087fd81b54f5f6afc4015210dbe5fe7fe6ce40b6
Steven Deutsch <stevensdeutsch@yahoo.com>
2017-04-04 19:13:55 -0500

    Add DatabaseResponse

---
+++ Nora/DatabaseResponse.swift
@@ -0,0 +1,46 @@
+//
+//  DatabaseResponse.swift
+//  Nora
+//
+//  Created by Steven on 4/4/17.
+//  Copyright © 2017 NoraFirebase. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+// MARK: - JSONDecodeable
+
+public protocol JSONDecodeable {
+    
+    init?(json: JSON)
+    
+}
+
+// MARK: - SnapshotDecodeable
+
+public protocol SnapshotDecodeable {
+    
+    init?(snapshot: FIRDataSnapshot)
+    
+}
+
+// MARK: - DatabaseResponse
+
+
+public struct DatabaseResponse {
+    
+    public let snapshot: FIRDataSnapshot?
+    public let reference: FIRDatabaseReference
+    public let isCommitted: Bool
+}
+
+public extension DatabaseResponse {
+    
+    init(reference: FIRDatabaseReference, snapshot: FIRDataSnapshot? = nil, isCommitted: Bool = false) {
+        self.reference = reference
+        self.snapshot = snapshot
+        self.isCommitted = isCommitted
+    }
+    
+}
